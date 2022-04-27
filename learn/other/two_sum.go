2c4c354cae8cc25567030999e81489b6350bbd72
lisong <lisong@cdsunrise.net>
2022-04-27 17:18:44 +0800

    soled 1. Two Sum with One-pass Hash Table

--- learn/other/two_sum.go
+++ learn/other/two_sum.go
@@ -3,14 +3,12 @@ package other
 func twoSum(nums []int, target int) []int {
 	m := make(map[int]int)
 	for i, v := range nums {
-		m[v] = i
-	}
-	for i, v := range nums {
 		s := target - v
 		j, ok := m[s]
-		if ok && i != j {
-			return []int{i, j}
+		if ok {
+			return []int{j, i}
 		}
+		m[v] = i
 	}
 	return []int{-1, -1}
 }
