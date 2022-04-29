7976cccb9908700ea4e81b4e3fd78a28510f4629
lisong <lisong@cdsunrise.net>
2022-04-29 10:51:58 +0800

    try 50. Pow(x, n) v3

--- learn/divide_conquer/powxn.go
+++ learn/divide_conquer/powxn.go
@@ -1,16 +1,19 @@
 package divide_conquer
 
 func myPow(x float64, n int) float64 {
-	return pow(x, n)
+	cache := make(map[int]float64)
+	cache[0] = 1.0
+	cache[1] = x
+	return pow(cache, x, n)
 }
 
-func pow(x float64, n int) float64 {
-	if n == 0 {
-		return 1
-	}
-	if n == 1 {
-		return x
+func pow(cache map[int]float64, x float64, n int) float64 {
+	r, ok := cache[n]
+	if ok {
+		return r
 	}
 	h := n / 2
-	return pow(x, h) * pow(x, n-h)
+	x1 := pow(cache, x, h) * pow(cache, x, n-h)
+	cache[n] = x1
+	return x1
 }
